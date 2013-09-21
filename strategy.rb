955fcff934ec6881a731639be0aa38f5aa05a45e
Russ Bernhardt <russ@rdkl.us>
2013-09-21 23:39:07 +0000

    Meh. Changed chance to act mean.

--- strategy.rb
+++ strategy.rb
@@ -5,7 +5,8 @@ extend Aggressive
 extend Defensive
 
 @chooser = Proc.new do
-  rand() <= 0.5 ? act_aggressively : act_defensively
+  # act mean 30% of the time.
+  rand() <= 0.3 ? act_aggressively : act_defensively
 end
 
 on_turn do
